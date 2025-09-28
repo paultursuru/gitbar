249272533d7aeb1a75b0b6e85c7c6799bb3bc3fc
paul lahana <paul.lahana@gmail.com>
2025-09-28 16:32:07 +0200

    Refactor GitHub CLI path detection in Setup class
    - Enhanced the `gh_path` method to check for the GitHub CLI installation using common paths if the initial command fails.
    - Improved error handling to provide clearer messages when the GitHub CLI is not found.

--- gitbar_app/config/setup.rb
+++ gitbar_app/config/setup.rb
@@ -4,11 +4,22 @@
 # of the Gitbar application. It handles loading settings, ensuring necessary
 # configurations are present, and preparing the application environment.
 class Setup
-  # check weither GH is installed or not and it's path
+  # check weither GH is installed with brew or local
   def self.gh_path
-    gh_cli_path = `command -v gh`.strip
+    # which returns "whatever not found" if whatever is not installed.
+    gh_cli_path = `which gh`.strip
 
-    raise 'GitHub CLI (gh) not found. Please install it to use Gitbar.' if gh_cli_path.empty?
+    # In case "which" does not work, we test two common paths for gh
+    if gh_cli_path.empty? || gh_cli_path.match?(/not found/)
+      if File.exist?('/opt/homebrew/bin/gh')
+        gh_cli_path = '/opt/homebrew/bin/gh'
+      elsif File.exist?('/usr/local/bin/gh')
+        gh_cli_path = '/usr/local/bin/gh'
+      end
+    end
+
+    # If gh_cli_path is still empty..
+    raise 'GitHub CLI (gh) not found. Please install it to use Gitbar.' if gh_cli_path.empty? || gh_cli_path.match?(/not found/)
 
     gh_cli_path
   end
@@ -18,14 +29,14 @@ class Setup
     whoami.strip
   end
 
-  # Loading repositories list
   def self.load_settings
+    # Loading repositories list
     settings_path = File.join(__dir__, 'settings.json')
     JSON.parse(File.read(settings_path))
   end
 
-  # Loading the last persisted view in case the connection is lost.
   def self.load_full_view_array
+    # Loading the last persisted view in case the connection is lost.
     full_view_path = File.join(__dir__, 'view.json')
     File.write(full_view_path, []) unless File.exist?(full_view_path)
     JSON.parse(File.read(full_view_path))
