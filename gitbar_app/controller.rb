e4b431d6919fc6d79204bb4eb9b6af1bca9968f6
paul lahana <paul.lahana@gmail.com>
2026-01-20 20:16:48 +0100

    Comments and wording.

--- gitbar_app/controller.rb
+++ gitbar_app/controller.rb
@@ -7,6 +7,7 @@ class Controller
     @repositories_data = repositories_data || []
   end
 
+  # Fetching repositories from the data
   def fetch_repositories
     @repositories_data.map do |repository_data|
       Repository.new(repository_data: repository_data)
@@ -14,7 +15,7 @@ class Controller
   end
 
   # Storing the whole created view inside view.json, in case connection is lost
-  def persists_view(view:)
+  def persist_view(view:)
     path = File.join(__dir__, 'config', 'view.json')
     File.write(path, view.full_view_array.to_json)
   end
